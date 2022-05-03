c85fe665316c5d1683e5f0c5641db1a7e4f8026a
Artem <artem.ivanchikov.99@mail.ru>
2022-05-03 16:12:31 +0600

    added email page

---
+++ src/main/java/com/job4you/model/Email.java
@@ -0,0 +1,14 @@
+package com.job4you.model;
+
+public class Email {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
